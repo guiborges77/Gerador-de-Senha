6708bb62d9c36135cde4aba8a39ec864eeab3f7d
Guilherme Borges <149341165+guiborges77@users.noreply.github.com>
2024-01-16 12:19:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
+//Variáveis
 let sliderElement = document.querySelector("#slider");
 let buttonElement = document.querySelector("#button");
-
 let sizePassword = document.querySelector("#valor");
 let password = document.querySelector("#password");
-
 let containerPassword = document.querySelector("#container-password");
-
 let charset = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789!@#$%";
-
 let novaSenha = " ";
 
 sizePassword.innerHTML = sliderElement.value;
